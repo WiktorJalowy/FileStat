b4b3da2f972cff3f3fd103f97ffe8a3ba5d01fa7
Witas <wiktor.jalowy@o2.pl>
2022-02-21 13:35:50 +0100

    added current_path initialization

--- main.cpp
+++ main.cpp
@@ -1,4 +1,22 @@
+#include <filesystem>
+#include <iostream>
+
 int main(int argc, char **argv)
 {
+    if (argc != 2)
+    {
+        std::cout << "Niepoprawna ilosc argumentow!" << std::endl;
+        return -1;
+    }
+
+    try
+    {
+        std::filesystem::current_path(argv[1]);
+    }
+    catch (std::exception e)
+    {
+        std::cout << e.what() << std::endl;
+        return -1;
+    }
     return 0;
 }

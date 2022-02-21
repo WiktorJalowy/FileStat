93d392c0db9eb4bcd3d25a24dd9c7e59ab5929a7
Witas <wiktor.jalowy@o2.pl>
2022-02-21 14:04:26 +0100

    added line count

--- main.cpp
+++ main.cpp
@@ -1,4 +1,5 @@
 #include <filesystem>
+#include <fstream>
 #include <iostream>
 
 int main(int argc, char **argv)
@@ -19,13 +20,21 @@ int main(int argc, char **argv)
         return -1;
     }
     int numOfFiles = 0;
+    int numOfLines = 0;
     for (const auto &dir_entry : std::filesystem::recursive_directory_iterator("."))
     {
         if (dir_entry.is_regular_file())
         {
             numOfFiles++;
+            std::ifstream file(dir_entry.path().c_str());
+            std::string line;
+            while (std::getline(file, line))
+            {
+                numOfLines++;
+            }
         }
     }
     std::cout << numOfFiles << std::endl;
+    std::cout << numOfLines << std::endl;
     return 0;
 }

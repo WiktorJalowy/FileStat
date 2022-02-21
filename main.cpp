2c904462971f7749086357aa011421f8a135f6dc
Witas <wiktor.jalowy@o2.pl>
2022-02-21 14:50:56 +0100

    removed unnecessary checks

--- main.cpp
+++ main.cpp
@@ -4,12 +4,6 @@
 
 int main(int argc, char **argv)
 {
-    if (argc != 2)
-    {
-        std::cout << "Niepoprawna ilosc argumentow!" << std::endl;
-        return -1;
-    }
-
     try
     {
         std::filesystem::current_path(argv[1]);
